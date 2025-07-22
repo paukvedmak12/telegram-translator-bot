800faca44a03ee99859666a17d0330d21b85433f
paukvedmak12 <vladis322@gmail.com>
2025-07-22 15:31:25 +0300

    Add .env and config.py for api keys

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 # config.py
 
 # Configuration settings for the Telegram Translator Bot
+from dotenv import load_dotenv
+import os
 
-TELEGRAM_BOT_TOKEN = "8088624473:AAHLCIGS3Th1hU8WagIjGYaIz9DdQLljttk"
-TRANSLATION_API_KEY = "881b75e2-76ce-4a86-9074-82fe253ab695:fx"
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TILDE_API_KEY = os.getenv("TILDE_API_KEY")
 
 # Supported languages
 SUPPORTED_LANGUAGES = ["en", "es", "fr", "de", "ru", "zh"]
